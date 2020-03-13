eb1583de42ec5027c48031e260f03d364fe3fbde
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2020-03-13 14:24:25 +0900

    prototype実装

--- patterns/Prototype/index.php
+++ patterns/Prototype/index.php
@@ -6,6 +6,7 @@
  * PHPにはもともとcloneの機能があるのでそれを用いる
  * cloneの機能がない言語においては
  * 明示的に深いコピーをしたインスタンスを返す
+ * つまり、ディープコピーしないと予期せぬバグの原因になりますよということ
  * 
  * 
  */
@@ -61,4 +62,46 @@ class Child
 }
 
 
+function say($i)
+{
+  print "$i <br>";
+}
 
+
+// オリジナル作成
+
+$originalObj = new Prototype();
+$originalObj->hoge = "fuga";
+$cloneObj = clone $originalObj;
+say($originalObj->hoge === $cloneObj->hoge ? "OK" : "NG"); //OK
+
+
+// クローンにピヨを代入
+$cloneObj->hoge = "piyo";
+
+//クローンにはhogeがあって、
+// オリジナルにはpiyoがある
+say($originalObj->hoge == $cloneObj->hoge  ? "OK" : "NG"); //NG;
+
+var_dump($originalObj->hoge);
+var_dump($cloneObj->hoge);
+
+$originalObj->child = new Child();
+$originalObj->child->xyzzy = "vim";
+$cloneObj  = clone $originalObj;
+say($originalObj->child->xyzzy == $cloneObj->child->xyzzy ? "OK" : "NG"); //OK
+
+// cloneのchildのxyzzyにemacsを代入
+
+$cloneObj->child->xyzzy = "emacs";
+
+// オリジナルとクローンが正しくディープコピーされている
+// マジックメソッドcloneを上書きしておかないと
+// オリジナルもemacsになるので気をつける
+// 以下は、Childには__cloneしてないので、
+// childのプロパティがシャローコピーされてしまった例
+say($originalObj->child->xyzzy == "vim" ? "OK" : "NG"); // NG
+say($cloneObj->child->xyzzy == "emacs" ? "OK" : "NG"); //OK
+
+var_dump($originalObj->child);
+var_dump($cloneObj->child);
