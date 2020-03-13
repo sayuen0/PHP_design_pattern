214b2b977c2028ef6f20f40fc6c698df2b955c10
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2020-03-13 14:04:20 +0900

    リンクに作成したパターンを追加

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
   <p>参考：<a href="http://www.dezapatan.com/">でざぱたん公式Webページ | TOP</a>
   </p>
   <ol>
-    <li>
-      <a href="patterns/Builder/index.php">Builderパターン</a>
-
-    </li>
+    <li><a href="patterns/Builder/">Builderパターン</a></li>
+    <li><a href="patterns/Command/">Commandパターン</a></li>
+    <li><a href="patterns/Proxy/">Proxyパターン</a></li>
+    <li><a href="patterns/TemplateMethod/">TemplateMethodパターン</a></li>  
   </ol>
 
 
