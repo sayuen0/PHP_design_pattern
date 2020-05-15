241bb4c0e39e3c197c7587db534a839adb53dff9
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2020-05-15 22:59:16 +0900

    delete: bookディレクトリ
    本番にはいらない

--- _book/Procfile
+++
@@ -1,4 +0,0 @@
-web: ./node_modules/gitbook-cli/bin/gitbook.js serve --port 4000
-
-
-
--- _book/patterns/Composite/remove.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "index.php";
-
-$dir = new RemovableDirectory("tmp/composite");
-
-var_dump($dir->remove());
-
-?>
-<a href="make.php">生成</a>
